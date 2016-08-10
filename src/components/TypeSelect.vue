b866dba2c6a9ce77ed3a8e9727f261e624a4602f
George Karagiorgos <georgekar94@gmail.com>
2016-08-10 19:44:40 +0300

    First commit. Seems to be working but still a heavy WIP.

---
+++ src/components/TypeSelect.vue
@@ -0,0 +1,15 @@
+<template>
+
+<script>
+export default {
+  props:['name']
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
